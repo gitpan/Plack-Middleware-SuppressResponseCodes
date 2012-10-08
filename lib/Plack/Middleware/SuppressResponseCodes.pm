b023983e9db9e4472f2432cee326ea682b2d84df
Jakob Voß <jakob.voss@gbv.de>
2012-10-08 09:25:37 +0000

    Import of VOJ/Plack-Middleware-SuppressResponseCodes-0.2 from CPAN.
    gitpan-cpan-distribution: Plack-Middleware-SuppressResponseCodes
    gitpan-cpan-version:      0.2
    gitpan-cpan-path:         VOJ/Plack-Middleware-SuppressResponseCodes-0.2.tar.gz
    gitpan-cpan-author:       VOJ
    gitpan-cpan-maturity:     released

--- lib/Plack/Middleware/SuppressResponseCodes.pm
+++ lib/Plack/Middleware/SuppressResponseCodes.pm
@@ -1,22 +1,25 @@
 package Plack::Middleware::SuppressResponseCodes;
 {
-  $Plack::Middleware::SuppressResponseCodes::VERSION = '0.1';
+  $Plack::Middleware::SuppressResponseCodes::VERSION = '0.2';
 }
 #ABSTRACT: Return HTTP Status code 200 for errors on request
 
 use strict;
 use parent qw(Plack::Middleware);
 
+use Plack::Util;
+
 sub call {
     my($self, $env) = @_;
     my $res = $self->app->($env);
-    $self->response_cb($res, sub {
-		if ( $res->[0] =~ /^[45]../ and
-        	 $env->{QUERY_STRING} =~ /(?:^|&)suppress_response_codes(=([^&]+))?/ 
-		     and !($1 and $2 =~ /^(0|false)$/) ) {
-			$res->[0] = 200;
-		}
-	});
+    Plack::Util::response_cb($res, sub {
+        my $res = shift;
+        if ( $res->[0] =~ /^[45]../ and
+             $env->{QUERY_STRING} =~ /(?:^|&)suppress_response_codes(=([^&]+))?/ 
+             and !($1 and $2 =~ /^(0|false)$/) ) {
+            $res->[0] = 200;
+        }
+    });
 }
 
 1;
@@ -32,25 +35,30 @@ Plack::Middleware::SuppressResponseCodes - Return HTTP Status code 200 for error
 
 =head1 VERSION
 
-version 0.1
+version 0.2
 
 =head1 SYNOPSIS
 
     use Plack::Builder;
 
     builder {
-	    enable 'SuppressResponseCodes'
-	    $app
-	};
+        enable 'SuppressResponseCodes';
+        $app;
+    };
 
 =head1 DESCRIPTION
 
-Plack::Middleware::SuppressResponseCodes sets the HTTP status code of a
-response to 200 if the response is an error (status code 4xx or 5xx) and the
-query parameter C<suppress_response_codes> is present with any value except
-C<0> or C<false>. This behaviour is useful for clients that cannot handle HTTP
-errors.  It has been implemented in popular APIs such as Twitter and Microsoft
-Live.
+Plack::Middleware::SuppressResponseCodes modifies error responses (PSGI
+response with HTTP status code 4xx or 5xx) if the query parameter
+C<suppress_response_codes> is present with any value except C<0> or C<false>.
+The status code is set to 200 in this case. This behaviour is useful for
+clients that cannot handle HTTP errors.  It has also been implemented in
+popular APIs such as Twitter and Microsoft Live.
+
+=head1 SEE ALSO
+
+One should think about embedding the status code in the response body when
+using this middleware, for instance with L<Plack::Middleware::JSONP::Headers>.
 
 =encoding utf8
 
